ddc1fa39aab72bc53f76b7230e561384e407fbc9
xwu36 <xwu36@buffalo.edu>
2017-01-13 17:20:50 -0500

    validator

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var session = require('express-session');
-
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +25,12 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session( { secret: 'mysupersecret', resave:false, saveUninitialized: false } ) );
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 var csrf = require('csurf');
+var passport = require('passport');
+var Product = require('../models/product');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -20,11 +22,19 @@ router.get('/', function(req, res, next) {
 
 router.get('/user/signup', function(req, res, next){
 	//res.send('singup page');
-	res.render('user/signup', {csrfToken : req.csrfToken()});
+	var messages = req.flash('error');
+	res.render('user/signup', {csrfToken : req.csrfToken(), messages : messages});
 });
 
-router.post('/user/signup', function(req, res, next){
-	res.redirect('/');
-})
+router.post('/user/signup', passport.authenticate('local.signup', {
+	successRedirect: '/user/profile',
+	failureRedirect: '/user/signup',
+	failureFlash: true
+}));
+
+router.get('/user/profile', function(req, res, next){
+	//res.send('success');
+	res.render('user/profile');
+});
 
 module.exports = router;
